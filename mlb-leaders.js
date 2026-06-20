aadbf23393e4449e5e980ebfa3311a6da6a6678e
ari20260322 <arise.yuuki433005@gmail.com>
2026-06-20 21:53:53 +0900

    Fix message string formatting in main function
    Remove newline character from message string initialization.

--- mlb-leaders.js
+++ mlb-leaders.js
@@ -31,7 +31,7 @@ const LEAGUES = [
 ];
 
 async function main() {
-  let message = `🏆 **MLB 現在のタイトルランキング トップ3** 🏆\n`;
+  let message = `🏆 **MLB 現在のタイトルランキング トップ3** 🏆`;
   for (const league of LEAGUES) {
     message += `\n### ${league.name}\n`;
 
